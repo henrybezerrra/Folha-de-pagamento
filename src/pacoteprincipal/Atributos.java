10ae6e56302960c5f67f84ba2c3fc85fe68e7291
Murilo Henrique Bezerra Silva <henrybezerra10@outlook.com>
2017-11-28 09:12:18 -0300

    ...

--- src/pacoteprincipal/Atributos.java
+++ src/pacoteprincipal/Atributos.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pacoteprincipal;
 
 /**
@@ -10,5 +5,71 @@ package pacoteprincipal;
  * @author Murilo Henrique
  */
 public class Atributos {
+    
+    private double salFamilia; // Salario Familia
+    private double sal; // Salario
+    private double qf; // Quantidade de filho
+    private double inss; // INSS
+    private double irrf; // IRRF
+    private double inssAli; // INSS
+    private double irrfAli; // IRRF
+
+    public double getSalFamilia() {
+        return salFamilia;
+    }
+
+    public void setSalFamilia(double salFamilia) {
+        this.salFamilia = salFamilia;
+    }
+
+    public double getSal() {
+        return sal;
+    }
+
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
+
+    public double getQf() {
+        return qf;
+    }
+
+    public void setQf(double qf) {
+        this.qf = qf;
+    }
+
+    public double getInss() {
+        return inss;
+    }
+
+    public void setInss(double inss) {
+        this.inss = inss;
+    }
+
+    public double getIrrf() {
+        return irrf;
+    }
+
+    public void setIrrf(double irrf) {
+        this.irrf = irrf;
+    }
+
+    public double getInssAli() {
+        return inssAli;
+    }
+
+    public void setInssAli(double inssAli) {
+        this.inssAli = inssAli;
+    }
+
+    public double getIrrfAli() {
+        return irrfAli;
+    }
+
+    public void setIrrfAli(double irrfAli) {
+        this.irrfAli = irrfAli;
+    }
+    
+    
     
 }
--- src/pacoteprincipal/SalarioFamilia.java
+++ src/pacoteprincipal/SalarioFamilia.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pacoteprincipal;
 
 /**
@@ -10,5 +5,10 @@ package pacoteprincipal;
  * @author Murilo Henrique
  */
 public class SalarioFamilia {
+
+    private double valorA= 29.43;
+    private double valorB= 20.74;
+    
+   
     
 }
